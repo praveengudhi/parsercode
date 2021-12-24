945ffe208909884ea6e3c9e2add45dfb370e0f63
unknown <gudhipraveen@gmail.com>
2021-12-24 16:16:39 -0600

    Added comments

--- Parser.py
+++ Parser.py
@@ -1,45 +1,55 @@
 import json
-class parser(object):
-    def search(self, values, searchFor):           
-        for k in values:
-            for v in values[k]:
-                if searchFor in v:
-                    return k
-        return None
-    
-    def stbool(self, v):
-        if(v.lower() in "yes", "true", "on"):
-            return "true"
-        elif (v.lower() in "false","no","off"):
-            return "false"
+# Parser class to sear
 
-    
+class parser(object):
+    # Funtion for Seacrch  Key and Values from  Dictionary
+    def searchKeyValues(self, values, searchFor):           
+        for key in values:
+            for val in values[key]:
+                if searchFor in val:
+                    return key
+        return None
+    #  Funtion for Boolean values Loo up
+    def stbool(self, value):
+        if(value.lower() in "yes", "true", "on"):
+            return "true"
+        elif (value.lower() in "false","no","off"):
+            return "false"
+    # Method for parsing the configuration file
     def configparse(self, config_in, config_json):
         self.dictobject ={}
         self.config_names = {'strings' :['host ','user','log_file_path'],
                 'bools':['verbose','test_mode','debug_mode','send_notifications'],
                 'digits':['server_id'],
                 'decimals':['server_load_alarm']}
-        with open('config.txt', 'rt') as fil:
-            for str in fil:
-                str = str.rstrip('\n')
-                d = dict(x.split("=") for x in str.split(":"))        
-                for key, val in d.items():
-                    key.replace(" ","")
-                    val.replace(" ","")          
-                    returnkey = self.search(self.config_names, key)
-                    if returnkey=='bools':
-                        returnval = self.stbool(val)
-                        print("booreturn", returnval)
-                        self.dictobject.update({key:returnval})
-                    elif returnkey == 'strings':
-                        self.dictobject.update({key:val})                                    
-                    elif returnkey == 'digits':
-                        self.dictobject.update({key:int(val)})
-                    elif returnkey == 'decimals':
-                        self.dictobject.update({key:float(val)})
+        # Read configuration file 
+        with open('config.ini', 'rt') as file:
+            for line in file:
+                # Read the comments section lines
+                line = line.rstrip('\n')                
+                if  line.lstrip().startswith("#"):                    
+                        self.dictobject.update({"comment":line})
+                        continue
+                else:
+                    # Dictionary for splitting the Key and Values
+                    d = dict(x.split("=") for x in line.split(":")) 
+                    #
+                    for key, val in d.items():
+                        key.replace(" ","")
+                        val.replace(" ","")
+                        #  Check for the values in strings
+                        returnkey = self.searchKeyValues(self.config_names, key)
+                        if returnkey=='bools':
+                            returnval = self.stbool(val)                            
+                            self.dictobject.update({key:returnval})
+                        elif returnkey == 'strings':
+                            self.dictobject.update({key:val})                                    
+                        elif returnkey == 'digits':
+                            self.dictobject.update({key:int(val)})
+                        elif returnkey == 'decimals':
+                            self.dictobject.update({key:float(val)})
 
             print("dict", self.dictobject)
             with open(config_json, 'w') as fp:
-                json.dump(self.dictobject, fp, sort_keys=True, indent=2)
+                json.dump(self.dictobject, fp,  indent=2)
                 
--- callparse.py
+++ callparse.py
@@ -1,13 +1,20 @@
 from Parser import parser
 
+##
+##class textparse(object):
+##    def __init__(self):
+##        print("init")
+##    def textmain(self):
+##        self.parser().configparse("config.ini","config.json")
+##
+##
+##if __name__ == '__main__':
+##    textparse = textparse()
+##    textparse.textmain()
 
-class textparse(object):
-    def __init__(self):
-        print("init")
-    def textmain(self):
-        self.parser().configparse("config.ini","config.json")
 
+def textmain():
+    parser().configparse("config.ini","config.json")
+    
+textmain()
 
-if __name__ == '__main__':
-    textparse = textparse()
-    textparse.textmain()
