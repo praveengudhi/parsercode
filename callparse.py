7d58911c04116aaaeb94dff0a5933dc4b36c1ee8
unknown <gudhipraveen@gmail.com>
2021-12-24 14:52:30 -0600

    changes to call parser class

--- callparse.py
+++ callparse.py
@@ -2,9 +2,12 @@ from Parser import parser
 
 
 class textparse(object):
+    def __init__(self):
+        print("init")
     def textmain(self):
-        self.parser().configparse("config.txt","config.json")
+        self.parser().configparse("config.ini","config.json")
 
 
 if __name__ == '__main__':
-    textparse.textmain(self)
+    textparse = textparse()
+    textparse.textmain()
